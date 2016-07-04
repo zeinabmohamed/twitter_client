4d2e7715436ae0d78b8341d39f35d18915ce0886
zeinabmohamed <zeinabmohamed22@gmail.com>
2016-07-04 10:18:22 +0200

    handle login with twitter

--- app/src/main/java/com/gamila/zm/twitterclient/view/activity/AuthrizationActivity.java
+++ app/src/main/java/com/gamila/zm/twitterclient/view/activity/AuthrizationActivity.java
@@ -1,8 +1,10 @@
 package com.gamila.zm.twitterclient.view.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -20,5 +22,17 @@ public class AuthrizationActivity extends AppCompatActivity {
 
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        for (Fragment fragment : getSupportFragmentManager().getFragments()) {
+            fragment.onActivityResult(requestCode, resultCode, data);
+        }
+
+    }
+
+
 
 }
--- app/src/main/java/com/gamila/zm/twitterclient/view/activity/LoginFragment.java
+++ app/src/main/java/com/gamila/zm/twitterclient/view/activity/LoginFragment.java
@@ -1,11 +1,13 @@
 package com.gamila.zm.twitterclient.view.activity;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.gamila.zm.twitterclient.R;
 import com.twitter.sdk.android.core.Callback;
@@ -51,6 +53,13 @@ public class LoginFragment extends Fragment {
             @Override
             public void success(Result<TwitterSession> result) {
 
+                // The TwitterSession is also available through:
+                // Twitter.getInstance().core.getSessionManager().getActiveSession()
+                TwitterSession session = result.data;
+                // TODO: Remove toast and use the TwitterSession's userID
+                // with your app's user model
+                String msg = "@" + session.getUserName() + " logged in! (#" + session.getUserId() + ")";
+                Toast.makeText(getActivity(), msg, Toast.LENGTH_LONG).show();
 
                 Timber.i("success");
             }
@@ -63,5 +72,16 @@ public class LoginFragment extends Fragment {
             }
         });
 
+            }
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Make sure that the loginButton hears the result from any
+        // Activity that it triggered.
+        loginTwitterButton.onActivityResult(requestCode, resultCode, data);
     }
+
 }
